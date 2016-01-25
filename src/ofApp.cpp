6d660e1aa9f7c010793d632a9cdf26af66d4143e
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2016-01-25 21:43:03 +0100

    [IMP] 16 -> 32

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,20 +3,21 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     width = 20;
-    ofSetFrameRate(20);
+    ofSetFrameRate(10);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     // Faire memcpy
-    memcpy(grid[1], grid[0], sizeof(bool)*15*32);
+    memcpy(grid[1], grid[0], sizeof(bool)*31*32);
     for(int i=0; i<32; i++)
         grid[0][i] = rand()%2;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    for(int i=0; i<16; i++){
+    ofBackground(0);
+    for(int i=0; i<32; i++){
         for (int j=0; j<32; j++){
             if (grid[i][j] == true){
                 ofDrawRectangle(0+j*(width+3), 0+i*(width+3), width, width);
--- src/ofApp.h
+++ src/ofApp.h
@@ -20,6 +20,6 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-    bool grid[16][32];
+    bool grid[32][32];
     int width;
 };
